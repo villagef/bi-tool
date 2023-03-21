ea1efde6ca24c3d93f5b8eca079026c57c97cf21
villagef <filwyd123@gmail.com>
2023-03-21 22:00:34 +0100

    feat: increase uploaded file limit and add uniq id

--- server/server.js
+++ server/server.js
@@ -1,37 +1,44 @@
 const express = require("express");
 const jsonfile = require("jsonfile");
 const cors = require("cors");
+const { v4: uuid } = require("uuid");
 
 const app = express();
 const PORT = 8080;
-const FILE_NAME = "data.json";
+const DATABASE_NAME = "data.json";
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "1000mb" }));
+app.use(express.urlencoded({ limit: "1000mb" }));
 
-// Endpoint GET, zwracający wszystkie dane
+// Endpoint GET
 app.get("/api", async (req, res) => {
   try {
-    const data = await jsonfile.readFile(FILE_NAME);
+    const data = await jsonfile.readFile(DATABASE_NAME);
     res.send(data);
   } catch (err) {
     console.log(err);
-    res.status(500).send("Błąd serwera");
+    res.status(500).send("Server failed!");
   }
 });
 
-// Endpoint POST, zapisujący dane
+// Endpoint POST
 app.post("/api", async (req, res) => {
   const newData = req.body;
+  const newObject = {
+    id: uuid(),
+    uploadDate: new Date(),
+    ...newData,
+  };
   try {
-    const data = await jsonfile.readFile(FILE_NAME);
-    data.push(newData);
-    await jsonfile.writeFile(FILE_NAME, data);
-    res.send("Dane zostały zapisane");
+    const data = await jsonfile.readFile(DATABASE_NAME);
+    data.push(newObject);
+    await jsonfile.writeFile(DATABASE_NAME, data);
+    res.send("Data has been saved successfuly!");
   } catch (err) {
     console.log(err);
-    res.status(500).send("Błąd serwera");
+    res.status(500).send("Server failed!");
   }
 });
 
-app.listen(PORT, () => console.log(`Serwer działa na porcie ${PORT}`));
+app.listen(PORT, () => console.log(`Server works on port ${PORT}`));
