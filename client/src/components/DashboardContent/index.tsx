bc8dd4e69648dce68f390657fd92f8fb72109c01
villagef <filwyd123@gmail.com>
2023-08-20 15:05:30 +0200

    fix: grid item overlow

--- client/src/components/DashboardContent/index.tsx
+++ client/src/components/DashboardContent/index.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import { Layout, Responsive, WidthProvider } from "react-grid-layout";
 import _ from "lodash";
 import ContentBox from "components/ContentBox";
+import "./index.css";
 import "/node_modules/react-grid-layout/css/styles.css";
 import "/node_modules/react-resizable/css/styles.css";
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
@@ -16,15 +17,41 @@ type ItemCallback = (
 ) => void;
 
 const DashboardContent = () => {
-  const [mounted, setmounted] = useState(false);
   const [layouts, setLayouts] = useState<Layout[]>([
-    { i: "a", x: 0, y: 0, w: 100, h: 20 },
-    { i: "b", x: 1, y: 0, w: 100, h: 20 },
+    {
+      i: "a",
+      x: 0,
+      y: 0,
+      w: 192,
+      h: 20,
+      minW: 12,
+      minH: 8,
+      isBounded: true,
+      isDraggable: true,
+    },
+    {
+      i: "b",
+      x: 0,
+      y: 0,
+      w: 192,
+      h: 20,
+      minW: 12,
+      minH: 8,
+      isBounded: true,
+      isDraggable: true,
+    },
+    {
+      i: "c",
+      x: 0,
+      y: 0,
+      w: 192,
+      h: 20,
+      minW: 12,
+      minH: 8,
+      isBounded: true,
+      isDraggable: true,
+    },
   ]);
-
-  useEffect(() => {
-    setmounted(true);
-  }, [mounted]);
 
   const onDragStop = (
     layout: Layout[],
@@ -34,36 +61,41 @@ const DashboardContent = () => {
     e: MouseEvent,
     element: HTMLElement
   ): ItemCallback => {
-    console.log(layout, oldItem, newItem, placeholder, e, element);
+    // console.log(layout, oldItem, newItem, placeholder);
     return;
   };
 
   return (
     <ContentBox>
-      <div className="h-full w-full bg-[#f7fbff] shadow">
+      <div className="h-full w-full bg-[#f7fbff] p-2.5 shadow">
         <ResponsiveReactGridLayout
+          style={{
+            height: "100%",
+            width: "100%",
+            maxHeight: "100%",
+            maxWidth: "100%",
+            overflow: "hidden",
+          }}
           rowHeight={5}
-          containerPadding={[10, 10]}
+          containerPadding={[0, 0]}
           breakpoints={{ lg: 100 }}
-          cols={{ lg: 100 }}
-          autoSize={true}
+          cols={{ lg: 192 }}
           onDragStop={onDragStop}
-          onLayoutChange={() => setmounted(false)}
-          layouts={{ layouts }}
+          layouts={{ lg: layouts }}
           measureBeforeMount={true}
-          useCSSTransforms={mounted}
+          useCSSTransforms={true}
           compactType={null}
           preventCollision={true}
           isDroppable={true}
-          droppingItem={{ i: "xx", h: 2, w: 2 }}
+          // droppingItem={{ i: "xx", h: 2, w: 2 }}
         >
-          {layouts.map((itm, i) => (
+          {layouts.map((itm) => (
             <div
               key={itm.i}
               data-grid={itm}
-              className="flex items-center justify-center bg-gray-light"
+              className="flex max-w-full items-center justify-center bg-gray-light"
             >
-              {i}
+              {itm.i}
             </div>
           ))}
         </ResponsiveReactGridLayout>
